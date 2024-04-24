7703eacfdcc3dfe023edb34fed4e01befc07e73c
Alok Pratap Singh <alok.singh@sunstone.in>
2024-04-24 12:19:52 +0530

    Update the validator function

--- joiValidators.ts
+++ joiValidators.ts
@@ -1,41 +1,40 @@
-/**
- * Created By Alok Pratap Singh 23-04-2024
- */
+// /**
+//  * Created By Alok Pratap Singh 23-04-2024
+//  */
 
-const joi = require("joi");
-import { Request, Response, NextFunction } from 'express';
+import Joi from 'joi';
 
 export function validateFields(req: any, res: any, schema: any) {
-    const validation = schema.validate(req);
-    if (validation.error) {
-      const errorReason =
-        validation.error.details !== undefined
-          ? validation.error.details[0].message
-          : "Parameter missing or parameter type is wrong";
-      sendParameterMissingResponse(res, errorReason);
-      return false;
-    }
-    return true;
+  const validation = schema.validate(req);
+  if (validation.error) {
+    const errorReason =
+      validation.error.details !== undefined
+        ? validation.error.details[0].message
+        : "Parameter missing or parameter type is wrong";
+    sendParameterMissingResponse(res, errorReason);
+    return false;
   }
-  
-  export function sendParameterMissingResponse(res: any, errorReason: any) {
-    errorReason = errorReason || "Required Parameters are missing";
-    return res.status(400).json({
-      success: false,
-      message: errorReason,
-    });
+  return true;
+}
+
+export function sendParameterMissingResponse(res: any, errorReason: any) {
+  errorReason = errorReason || "Required Parameters are missing";
+  return res.status(400).json({
+    success: false,
+    message: errorReason,
+  });
+}
+
+export const joiHandler = (schema: Joi.Schema) => (req: any, res: any, next: any) => {
+  const data = {
+    ...(req?.body && Object.keys(req?.body).length > 0 ? req.body : {}),
+    ...(req?.files && Object.keys(req?.files).length > 0 ? req.files : {}),
+    ...(req?.query && Object.keys(req?.query).length > 0 ? req.query : {}),
+    ...(req?.params && Object.keys(req?.params).length > 0 ? req.params : {})
   }
-  
-  export const joiHandler = (schema: any) => (req: Request, res: Response, next: NextFunction) => {
-    const data = {
-        ...(req?.body && Object.keys(req?.body).length > 0 ? req.body : {}),
-        ...(req?.query && Object.keys(req?.query).length > 0 ? req.query : {}),
-        ...(req?.params && Object.keys(req?.params).length > 0 ? req.params : {})
-    }
-  
-    const validFields = validateFields(data, res, schema)
-    if (validFields) {
-        next()
-    }
+
+  const validFields = validateFields(data, res, schema)
+  if (validFields) {
+    next()
   }
-  
+}
--- leaguex_gaming/app/validators/feedRecomendationValidators.ts
+++ leaguex_gaming/app/validators/feedRecomendationValidators.ts
@@ -1,20 +1,5 @@
-import { Request, Response, NextFunction } from 'express';
 import Joi from 'joi'
 
-interface FeedRecommendationParams {
-  user_id: string;
-}
-
-const feedRecommendationSchema = Joi.object({
-  user_id: Joi.string().required()
+export const validateFeedRecommendation = Joi.object({
+  user_id: Joi.string().required(),
 });
-
-const validateFeedRecommendation = (req: Request<FeedRecommendationParams>, res: Response, next: NextFunction) => {
-  const { error } = feedRecommendationSchema.validate(req.params);
-  if (error) {
-    return res.status(400).json({ error: error.details[0].message });
-  }
-  next();
-};
-
-export { validateFeedRecommendation };
